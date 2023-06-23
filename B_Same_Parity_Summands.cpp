e518c8ddc6dc7901e7f242596a81515d742fa532
Iftekhar-Ahamed <iftekharahamed56@gmail.com>
2023-06-24 00:29:14 +0600

    24-06-23

---
+++ B_Same_Parity_Summands.cpp
@@ -0,0 +1,128 @@
+/***
+**      Bismillahir Rahmanir Rahim
+**              ALLAHU AKBAR
+**
+**     Author: Iftekhar Ahamed Siddiquee
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define read freopen("0_input.txt", "r", stdin)
+#define write freopen("0_output.txt", "w", stdout)
+#define ll long long
+#define INF (ll)1e16
+#define nn "\n"
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define test                 \
+    ll ct;                   \
+    cin >> ct, cin.ignore(); \
+    for (ll i = 1; i <= ct; i++)
+#define testcase cout << "Case " << i << ": ";
+#define Dpos(n) fixed << setprecision(n)
+int dRow[] = {-1, 0, 1, 0, 1, 1, -1, -1};
+int dCol[] = {0, 1, 0, -1, 1, -1, -1, 1};
+const double pi = acos(-1.0);
+const ll mod = 1e9 + 7;
+const ll mXs = 1e6;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    if (n % k == 0)
+    {
+        cout << "YES" << nn;
+        for (ll i = 0; i < k; i++)
+        {
+            cout << n / k << " ";
+        }
+        cout << nn;
+    }
+    else if (n % 2 == 0)
+    {
+        if ((k - 1) % 2 == 1 && 1 == (n - k - 1) % 2)
+        {
+            cout << "YES" << nn;
+            ll dici = 0;
+            for (ll i = 1; i < k; i++)
+            {
+                cout << 1 << " ";
+                dici += 1;
+            }
+            cout << n - dici << nn;
+        }
+        else if (k * 2 <= n)
+        {
+            cout << "YES" << nn;
+            ll dici = 0;
+            for (ll i = 1; i < k; i++)
+            {
+                cout << 2 << " ";
+                dici += 2;
+            }
+            cout << n - dici << nn;
+        }
+        else
+        {
+            cout << "NO" << nn;
+        }
+    }
+    else
+    {
+        if (k % 2 == 1 && k <= n)
+        {
+            cout << "YES" << nn;
+            ll dici = 0;
+            for (ll i = 1; i < k; i++)
+            {
+                cout << 1 << " ";
+                dici += 1;
+            }
+            cout << n - dici << nn;
+        }
+        else
+        {
+            cout << "NO" << nn;
+        }
+    }
+}
+
+int main()
+{
+#ifdef ONLINE_JUDGE
+    cin.tie(NULL), ios_base::sync_with_stdio(false);
+#endif
+    // read;
+    // write;
+    test
+    {
+        // testcase
+        solve();
+    }
+    return 0;
+}
+/*
+ * do something instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ */
+/* Final check before submit :
+1. array size or integer overflow,like uninitialised 0 index.
+2. Think twice,code once.check all possible counter test case.
+3. Be careful about corner case! n=1?k=1? something about 0?
+4. avoid stupid mistake!complexity(t/m)?precision error?submit same code twice?
+5. if got WA than remember that you are missing something common.
+*** Be confident!!! who knows? may be your one step back to AC ***
+4. minus mod ans=(ans-k+mod)%mod;
+*/
+/* IF WA???
+1. corner case! n=1?k=1? something about 0?
+2. check code(avoid stupid mistake)
+3. read the statement again(if there any missing point???)
+4. check the idea(check all possible counter test case again!!!)
+5. be calm,don't be panic!!!.(***this problem not going to decide your future***)
+6. don't waste too much time. move to next problem
+*/
