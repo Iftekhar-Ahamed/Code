f9d0014c94b189e9828dcc22df40a91b1f49134d
Iftekhar-Ahamed <iftekharahamed56@gmail.com>
2023-08-25 18:22:15 +0600

    ok

--- asik.cpp
+++ asik.cpp
@@ -1,12 +1,33 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+int a[1002];
+void precal()
+{
+  int ind = 1;
+  for (int num = 1; ind <= 1000; num++)
+  {
+    if (num % 3 == 0 || num % 10 == 3)
+    {
+    }
+    else
+    {
+      a[ind] = num;
+      ind++;
+    }
+  }
+}
+
 int main()
 {
-  int i=1;
-
-  kazKoro:
-  printf(" %d iftekhar,Asik\n",i);
-  i=i-1;
-  if(i>=-5)goto kazKoro;
+  int t;
+  precal();
+  cin >> t;
+  while (t--)
+  {
+    int num;
+    cin >> num;
+    cout << a[num] << endl;
+  }
 
   return 0;
 }
