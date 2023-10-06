e749de500e75e1cad945f5a120c0a9d66e6f2234
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2023-10-06 22:29:48 +0600

    ok

--- test.cpp
+++ test.cpp
@@ -1,71 +1,72 @@
-#include <bits/stdc++.h>
+/***
+**      Bismillahir Rahmanir Rahim
+**              ALLAHU AKBAR
+**
+**     Author: Iftekhar Ahamed Siddiquee
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
 using namespace std;
+
+#define read freopen("0_input.txt", "r", stdin)
+#define write freopen("0_output.txt", "w", stdout)
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
 #define ll long long
-#define endl "\n"
-#define INF 100000000
-#define abc "abcdefghijklmnopqrstuvwxyz"
-#define ABC "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define INF (ll)1e16
+#define EPS 1e-9
 #define PI 3.1415926535897932384626433832795
-// define sort function
-#define asort(a) sort(a.begin(), a.end())
-#define rsort(a) sort(a.rbegin(), a.rend())
-#define gsort(a) sort(a.begin(), a.end(), greater<int>())
-#define issort(a) is_sorted(a.begin(), a.end())
-#define areverse(a) reverse(a.begin(), a.end())
-#define Faster                        \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);
-// default define end
-bool atik(pair<int, int> &a, pair<int, int> &b)
-{
-    if (a.first == b.first)
-    {
-        return a.second < b.second;
-    }
-    return a.first > b.first;
-}
+#define mXs 1e6
+#define test          \
+    long long int ct; \
+    cin >> ct;        \
+    while (ct--)
+const double pi = acos(-1.0);
+const ll modulo = 1e9 + 7;
+int dRow[] = {-1, 0, 1, 0, 1, 1, -1, -1};
+int dCol[] = {0, 1, 0, -1, 1, -1, -1, 1};
+#define nn "\n"
+
 void solve()
 {
-    ll n, k;
-    cin >> n >> k;
-    vector<ll> ans;
-    vector<pair<int, int>> data;
-    for (int i = 0; i < n; i++)
-    {
-        ll x;
-        cin >> x;
-        x %= k;
-        if (x == 0)
-        {
-            ans.push_back(i + 1);
-        }
-        else
-        {
-            data.push_back({x, i + 1});
-        }
-    }
-    sort(data.begin(), data.end(), atik);
-
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << ans[i] << " ";
-    }
-    for (auto i : data)
-    {
-        cout << i.second << " ";
-    }
-    cout << endl;
+    cout << "HI" << nn;
 }
+
 int main()
 {
-    Faster;
-    ll t;
-    cin >> t;
-    while (t--)
+    cin.tie(NULL);
+    // read;
+    // write;
+    ios_base::sync_with_stdio(false);
+    test
     {
         solve();
     }
-    // solve();
     return 0;
 }
 /*
@@ -79,7 +80,7 @@ int main()
 3. Be careful about corner case! n=1?k=1? something about 0?
 4. avoid stupid mistake!complexity(t/m)?precision error?submit same code twice?
 5. if got WA than remember that you are missing something common.
-* Be confident!!! who knows? may be your one step back to AC *
+*** Be confident!!! who knows? may be your one step back to AC ***
 4. minus mod ans=(ans-k+mod)%mod;
 */
 /* IF WA???
@@ -87,6 +88,6 @@ int main()
 2. check code(avoid stupid mistake)
 3. read the statement again(if there any missing point???)
 4. check the idea(check all possible counter test case again!!!)
-5. be calm,don't be panic!!!.(**this problem not going to decide your future**)
+5. be calm,don't be panic!!!.(***this problem not going to decide your future***)
 6. don't waste too much time. move to next problem
 */
