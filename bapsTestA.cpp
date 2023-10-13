241e030269a399158874fb829bbc03f7e8464208
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2023-10-13 16:09:19 +0600

    ok

---
+++ bapsTestA.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    while (n--)
+        if (n)
+            printf("Hello World!\n");
+        else
+            printf("Hello World!\n\n");
+
+    return 0;
+}
